52274a8f8a4086f4bfd60d81e6a905482ba6d1f4
almir <almir.bij@gmail.com>
2022-12-25 14:08:45 +0100

    fix(webapp): make select controlled

--- apps/webapp/components/RegisterForm.tsx
+++ apps/webapp/components/RegisterForm.tsx
@@ -62,6 +62,7 @@ export default function Register() {
           <Select
             labelId="role"
             label="role"
+            value={registerBody?.role || ""}
             onChange={(event: SelectChangeEvent) => {
               setRegisterBody((current) => ({
                 ...current,
