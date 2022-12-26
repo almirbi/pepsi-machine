c9570c04556dfd683dad91c6b66b15b686d634e4
almir <almir.bij@gmail.com>
2022-12-26 21:19:45 +0100

    feat(webapp): remove body padding

--- apps/webapp/pages/_app.tsx
+++ apps/webapp/pages/_app.tsx
@@ -4,12 +4,20 @@ import type { AppProps } from "next/app";
 import { useMemo, useState } from "react";
 import ResponsiveAppBar from "../components/Navigation";
 import { UserContext } from "../components/UserContext";
+import { Global, css } from "@emotion/react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [user, setUser] = useState<User>();
   const value = useMemo(() => ({ user, setUser }), [user]);
   return (
     <UserContext.Provider value={value}>
+      <Global
+        styles={css`
+          body {
+            margin: 0;
+          }
+        `}
+      />
       <ResponsiveAppBar />
       <Container sx={{ mt: 5 }}>
         <Component {...pageProps} />
