6a26b2aecf4060806197311e45dcf99ac420dfe3
almir <almir.bij@gmail.com>
2022-12-27 19:59:50 +0100

    feat(webapp): always show scrollbar

--- apps/webapp/src/components/ProductList.tsx
+++ apps/webapp/src/components/ProductList.tsx
@@ -56,6 +56,15 @@ export const ProductList = ({ products, setProducts, onBuy }: Props) => {
           maxHeight: "65vh",
           overflow: "scroll",
           bgcolor: "background",
+          "::-webkit-scrollbar": {
+            "-webkit-appearance": "none",
+            width: "7px",
+          },
+          "::-webkit-scrollbar-thumb": {
+            "border-radius": "4px",
+            "background-color": "rgba(0, 0, 0, 0.5)",
+            "box-shadow": "0 0 1px rgba(255, 255, 255, 0.5)",
+          },
         }}
       >
         {products?.map((product) =>
