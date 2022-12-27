12e104152b16959d2180b0f61bf67d5b8fcb5280
almir <almir.bij@gmail.com>
2022-12-27 17:36:42 +0100

    build: unify eslint version; add auto sort

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -3,5 +3,8 @@ module.exports = {
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
   },
+  plugins: ["simple-import-sort"],
 };
