c557e604668b49e7b11629397ec83358ccdc73c5
almir <almir.bij@gmail.com>
2022-12-27 18:08:15 +0100

    fix(api): turn off no-unresolved

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -2,5 +2,6 @@ module.exports = {
   extends: ["turbo", "prettier", "plugin:import/recommended"],
   rules: {
     "import/no-duplicates": ["error", { considerQueryString: true }],
+    "import/no-unresolved": "off",
   },
 };
